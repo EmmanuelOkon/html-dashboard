e444fa934f672972fd8f71b142bfe7fdd21a8cc4
Emmanuel Okon <emmaudo27@gmail.com>
2023-07-01 08:56:20 +0100

    sidebar and navigation search bar

--- js/main.js
+++ js/main.js
@@ -5,3 +5,9 @@ menu.onclick = function() {
     sidebar.classList.toggle('active')
     mainContent.classList.toggle('active')
 }
+
+{/* <i class="fa fa-home" aria-hidden="true"></i>
+<i class="fa fa-graduation-cap" aria-hidden="true"></i>
+<i class="fa fa-lightbulb-o" aria-hidden="true"></i>
+<i class="fa fa-pie-chart" aria-hidden="true"></i>
+<i class="fa fa-cog" aria-hidden="true"></i> */}
