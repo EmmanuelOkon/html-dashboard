1d02f4ac051225f01b4c07c6a81f3e5411783862
Emmanuel Okon <emmaudo27@gmail.com>
2023-07-03 06:33:11 +0100

    dashboard layout without responsiveness

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
-let menu = document.querySelector('.menu')
-let sidebar = document.querySelector('.sidebar')
-let mainContent = document.querySelector('.main--content')
-menu.onclick = function() {
-    sidebar.classList.toggle('active')
-    mainContent.classList.toggle('active')
-}
-
-{/* <i class="fa fa-home" aria-hidden="true"></i>
-<i class="fa fa-graduation-cap" aria-hidden="true"></i>
-<i class="fa fa-lightbulb-o" aria-hidden="true"></i>
-<i class="fa fa-pie-chart" aria-hidden="true"></i>
-<i class="fa fa-cog" aria-hidden="true"></i> */}
+let menu = document.querySelector(".menu");
+let sidebar = document.querySelector(".sidebar");
+let mainContent = document.querySelector(".mainContent");
+menu.onclick = function () {
+  sidebar.classList.toggle("active");
+  mainContent.classList.toggle("active");
+};
