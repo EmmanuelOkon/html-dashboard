68fe661dd3aa4ef1715da4868f1d9775b91546fa
Emmanuel Okon <emmaudo27@gmail.com>
2023-07-03 06:50:22 +0100

    removed unused files

--- js/main.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector(".menu");
-let sidebar = document.querySelector(".sidebar");
-let mainContent = document.querySelector(".mainContent");
-menu.onclick = function () {
-  sidebar.classList.toggle("active");
-  mainContent.classList.toggle("active");
-};
